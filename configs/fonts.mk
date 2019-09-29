6b3c2172c173c116a112b647049a3cbed1fa0020
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-29 17:46:51 +0530

    404: fonts: Refactor fonts.mk to include all fonts
    Its way too tedious to type out each fonts' path to copy especially since 10 introduced so many styles
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I1c84ca37fea03ea7c832a003512701d0c48d10f8
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- configs/fonts.mk
+++ configs/fonts.mk
@@ -15,11 +15,9 @@
 #
 
 # Fonts
+LOCAL_PATH := vendor/404/fonts
 PRODUCT_COPY_FILES += \
-    vendor/404/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    vendor/404/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/404/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/404/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/404/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    vendor/404/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/404/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
