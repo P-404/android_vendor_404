dcf69ac64d7555c7a20f562b313c1cc2d9efbdf2
Danny Baumann <dannybaumann@web.de>
2020-05-20 15:51:13 +0200

    soong_config: Allow process-specific override of target SDK version
    Change-Id: I775e852c17af12d2233e644e3db10c10bcb20d19
    Signed-off-by: Udit Karode <udit.karode@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,11 +4,12 @@ add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)
 _json_contents := $(_json_contents)    "P404":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_str_omitempty, Target_init_vendor_lib,           $(TARGET_INIT_VENDOR_LIB))
-$(call add_json_str,  Target_shim_libs,                          $(TARGET_LD_SHIM_LIBS))
-$(call add_json_bool, Target_uses_color_metadata,                $(filter true,$(TARGET_USES_COLOR_METADATA)))
-$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
-$(call add_json_val_default, Bootloader_message_offset,          $(BOOTLOADER_MESSAGE_OFFSET), 0)
+$(call add_json_str_omitempty, Target_init_vendor_lib,              $(TARGET_INIT_VENDOR_LIB))
+$(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str,  Target_shim_libs,                             $(TARGET_LD_SHIM_LIBS))
+$(call add_json_bool, Target_uses_color_metadata,                   $(filter true,$(TARGET_USES_COLOR_METADATA)))
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits,    $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_val_default, Bootloader_message_offset,             $(BOOTLOADER_MESSAGE_OFFSET), 0)
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
