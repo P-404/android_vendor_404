dc814f6d0cde2011bc00e5dd744dcd1c36adf8d6
Akash <akashniki@gmail.com>
2020-06-12 21:03:26 +0200

    overlay: Add DarkBlue primary color

--- overlay/packages/Android.mk
+++ overlay/packages/Android.mk
@@ -22,7 +22,8 @@ LOCAL_REQUIRED_MODULES := \
     AccentColorOrangenessOverlay \
     AccentColorPurpleismOverlay \
     AccentColorSpotifyGreenOverlay \
-    PrimaryColorCharcoalGreyOverlay
+    PrimaryColorCharcoalGreyOverlay \
+    PrimaryColorDarkBlueOverlay
 
 include $(BUILD_PHONY_PACKAGE)
 include $(call first-makefiles-under,$(LOCAL_PATH))
