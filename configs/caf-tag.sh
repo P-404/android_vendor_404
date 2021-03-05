dadfb7484901042dc548a97a8cf1398b22985291
repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags <akashniki@gmail.com>
2021-03-05 11:44:28 +0000

    404: We want to show the QSSI revision instead of HAL one

--- configs/caf-tag.sh
+++ configs/caf-tag.sh
@@ -1,1 +1,1 @@
-grep --line-buffered -m 2 "refs/tags" manifest/default.xml | awk -F '/' '{print $3}' | awk -F '"' '{print $1}' | tail -n 1
+grep --line-buffered -m 3 "refs/tags" manifest/default.xml | awk -F '/' '{print $3}' | awk -F '"' '{print $1}' | tail -n 1
