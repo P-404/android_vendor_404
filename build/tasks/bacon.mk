c5dbe50f1c11d00b0b18721f476ccd9f1daff66c
Harsh  Pal <harsh@project404.us>
2022-04-11 06:32:59 +0000

    vendor: enhance packaging output
    - move the file then compare hash
    - introduce our new ascii
    - package related information

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -23,4 +23,18 @@ SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(P404_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(P404_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(P404_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(P404_TARGET_PACKAGE)" >&2
+	@echo -e ""
+	echo -e ${CL_BLD}"  ____            _           _     _  _    ___  _  _   "${CL_RST}
+	echo -e ${CL_BLD}" |  _ \ _ __ ___ (_) ___  ___| |_  | || |  / _ \| || |  "${CL_RST}
+	echo -e ${CL_BLD}" | |_) | '__/ _ \| |/ _ \/ __| __| | || |_| | | | || |_ "${CL_RST}
+	echo -e ${CL_BLD}" |  __/| | | (_) | |  __/ (__| |_  |__   _| |_| |__   _|"${CL_RST}
+	echo -e ${CL_BLD}" |_|   |_|  \___// |\___|\___|\__|    |_|  \___/   |_|  "${CL_RST}
+	echo -e ${CL_BLD}"               |__/                                     "${CL_RST}
+	@echo -e ""
+	@echo -e ${CL_BLD}${CL_CYN}"===============================-Package complete-==============================="${CL_RED}
+	@echo -e ${CL_BLD}${CL_CYN}"Zip: "${CL_YLW} $(P404_TARGET_PACKAGE)${CL_RST}
+	@echo -e ${CL_BLD}${CL_CYN}"SHA256: "${CL_YLW}" `cat $(P404_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	@echo -e ${CL_BLD}${CL_CYN}"Size:"${CL_YLW}" `du -sh $(P404_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	@echo -e ${CL_BLD}${CL_CYN}"Size in Int:"${CL_YLW}" `wc -c $(P404_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	@echo -e ${CL_BLD}${CL_CYN}"TimeStamp:"${CL_YLW}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	@echo -e ${CL_BLD}${CL_CYN}"================================================================================"${CL_RED}
