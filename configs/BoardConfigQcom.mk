3e54c35609f9eaa93a9f43201c44843c78106437
Harsh  Pal <harsh@project404.us>
2022-04-12 21:48:52 +0000

    vendor: set TARGET_COMPILE_WITH_MSM_KERNEL by default

--- configs/BoardConfigQcom.mk
+++ configs/BoardConfigQcom.mk
@@ -18,6 +18,9 @@ TARGET_USES_AOSP_FOR_AUDIO ?= false
 TARGET_USES_QCOM_MM_AUDIO := true
 TARGET_USES_ION := true
 
+# Tell HALs that we're compiling an AOSP build with an in-line kernel
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
 # Enable Media Extensions for HAL1 on Legacy Devices
 ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8937 msm8952 msm8953 msm8960 msm8974 msm8976 msm8992 msm8994 msm8996 msm8998 sdm660),true)
   TARGET_USES_MEDIA_EXTENSIONS := true
