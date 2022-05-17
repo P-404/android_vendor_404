144f76e98bd597ab87d0b72239a5f4d4d13dea4f
Harsh  Pal <harsh@project404.us>
2022-05-17 15:51:57 +0000

    vendor: add `WITH_GAPPS` flag

--- configs/common.mk
+++ configs/common.mk
@@ -115,6 +115,11 @@ PRODUCT_COPY_FILES += \
 $(foreach f,$(wildcard vendor/404/prebuilt/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
 
+# Gapps
+ifeq ($(WITH_GAPPS),true)
+    $(call inherit-product, vendor/gms/products/gms.mk)
+endif
+
 # Treble
 # Enable ALLOW_MISSING_DEPENDENCIES on Vendorless Builds
 ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
