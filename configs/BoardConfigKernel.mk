353a986a632b615c6226152c33d5c49ef5b11bbf
Chirayu Desai <chirayudesai1@gmail.com>
2022-07-08 03:33:22 +0530

    kernel: Don't print grep error messages when checking version
    Avoids:
    $ lunch lineage_sdk_phone_x86
    grep: build/make/target/board/emulator_x86/Makefile: No such file or directory (x4)
    
    Change-Id: Ie803d5ca93e6e7ae94625e7ee3ab0bf337e0515d

--- configs/BoardConfigKernel.mk
+++ configs/BoardConfigKernel.mk
@@ -57,8 +57,8 @@ endif
 TARGET_KERNEL_HEADERS ?= $(TARGET_KERNEL_SOURCE)
 KERNEL_SD_LLVM_SUPPORT ?= true
 
-KERNEL_VERSION := $(shell grep "^VERSION = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
-KERNEL_PATCHLEVEL := $(shell grep "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+KERNEL_VERSION := $(shell grep -s "^VERSION = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
+KERNEL_PATCHLEVEL := $(shell grep -s "^PATCHLEVEL = " $(TARGET_KERNEL_SOURCE)/Makefile | awk '{ print $$3 }')
 TARGET_KERNEL_VERSION ?= $(shell echo $(KERNEL_VERSION)"."$(KERNEL_PATCHLEVEL))
 
 # Tell HALs that we're compiling an AOSP build with an in-line kernel
