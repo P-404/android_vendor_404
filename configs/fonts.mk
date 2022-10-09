d9cac8364edd41b4ad9c5281dffbffd1b4243390
hpnightowl <iamhp2k@gmail.com>
2022-10-09 11:08:02 +0530

    vendor: move overlays to vendor/overlays/404
    Change-Id: I0fa1e258348d9194628318963216a82383652785

--- configs/fonts.mk
+++ configs/fonts.mk
@@ -15,7 +15,7 @@
 #
 
 # Fonts
-LOCAL_PATH := vendor/404/fonts
+LOCAL_PATH := vendor/overlays/404/fonts
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 
