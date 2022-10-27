7038923c59393fb5083967e6999b73d2d6b61f10
clhex <clhexftw@protonmail.com>
2022-10-27 01:06:49 -0400

    vendor: Build our fonts overlay
    Change-Id: Iba1d95e9f2c642f68575ab6f59d223670d87a5aa

--- configs/fonts.mk
+++ configs/fonts.mk
@@ -21,3 +21,11 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+PRODUCT_PACKAGES += \
+    FontInterOverlay \
+    FontOppoSansOverlay \
+    FontLexendOverlay \
+    FontQuicksandOverlay \
+    FontSwitzerOverlay \
+    FontGoogleSansOverlay
