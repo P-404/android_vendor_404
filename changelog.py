f3d5f6a05753d8f0043a061220c669e7d2aa0743
hpnightowl <iamhp2k@gmail.com>
2023-02-13 09:47:51 +0000

    vendor: add changelog script
    - everytime i need to see what changes i did and forgot to push stuff in source so let's do it now
    
    Change-Id: I478fa4ac6878756a0b3c2ac777c7769da759cf0b

---
+++ changelog.py
@@ -0,0 +1,30 @@
+import os
+import datetime
+
+Changelog = "Changelog.txt"
+device = os.environ.get('TARGET_PRODUCT', '').replace('p404_', '')
+out_dir = os.environ.get('OUT_DIR', './out')
+
+if os.path.isfile(Changelog):
+    os.remove(Changelog)
+
+with open(Changelog, 'w') as f:
+    for i in range(5):
+        after_date = (datetime.datetime.now() - datetime.timedelta(days=i)).strftime('%m-%d-%Y')
+        until_date = (datetime.datetime.now() - datetime.timedelta(days=i-1)).strftime('%m-%d-%Y')
+        f.write(f'================\n')
+        f.write(f'{until_date}\n')
+        f.write(f'================\n')
+        
+        with open('.repo/project.list') as projects:
+            for path in projects:
+                path = path.strip()
+                clog = os.popen(f'git --git-dir ./{path}/.git log --oneline --after={after_date} --until={until_date} --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7').read()
+                if clog:
+                    f.write(f'\n* {path}\n{clog}\n')
+        f.write('\n')
+
+os.popen(f'sed -i "s/project/   */g" {Changelog}')
+os.popen(f'cp {Changelog} {out_dir}/target/product/{device}/system/etc/')
+os.popen(f'cp {Changelog} {out_dir}/target/product/{device}/')
+print(f"Changelog file path: {os.path.abspath(Changelog)}")
